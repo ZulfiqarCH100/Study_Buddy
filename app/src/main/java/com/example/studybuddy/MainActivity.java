867e7b34256e88d36041ef3e7ec25a7f0501842a
Zulfiqar Chaudhry <zulfiqarchkonoha@gmail.com>
2021-06-13 21:42:44 +0500

    Bottom Navigation and Fragment switching set up. Sample fragments are added that would be updated in the future.

--- app/src/main/java/com/example/studybuddy/MainActivity.java
+++ app/src/main/java/com/example/studybuddy/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.studybuddy;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.motion.widget.MotionHelper;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,40 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpBottomNavigation();
+    }
+
+    private void setUpBottomNavigation(){
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        //Setting default fragment.
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new TodayFragment()).commit();
+
+        //Setting click listeners for the bottom navigation to switch fragments.
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment selectedFragment = null;
+                //Finding out which item was clicked.
+                switch (item.getItemId()) {
+                    case R.id.nav_today:
+                        selectedFragment = new TodayFragment();
+                        break;
+                    case R.id.nav_todo:
+                        selectedFragment = new TodoFragment();
+                        break;
+                    case R.id.nav_timetable:
+                        selectedFragment = new TimeTableFragment();
+                        break;
+                    case R.id.nav_others:
+                        selectedFragment = new OthersFragment();
+                        break;
+                }
+
+                //Switching the fragment to the clicked one
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                return true;
+            }
+        });
     }
 }
